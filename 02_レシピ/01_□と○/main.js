fda53d89da7be29831d5d961680f5f51a5f92d0d
kento0242 <y.kento.0242@gmail.com>
2021-10-09 22:03:51 +0900

    [ストップウォッチ]を学習しました。

--- 02_レシピ/01_□と○/main.js
+++ 02_レシピ/01_□と○/main.js
@@ -1,5 +1,20 @@
 const figure = document.getElementById("figure")
-
+let count = 0
 figure.onclick = function() {
-  figure.classList.toggle("rounded")
+  count += 1
+  console.log(count)
+  figure.classList.add("transition")
+  if (count % 3 == 1) {
+    figure.classList.add("rounded")
+    console.log("A")
+  } else if (count % 3 == 2) {
+    figure.classList.remove("spuare")
+    figure.classList.remove("rounded")
+    figure.classList.add("triangle")
+    console.log("B")
+  } else if (count % 3 == 0) {
+    figure.classList.remove("triangle")
+    figure.classList.add("square")
+    console.log("C")
+  }
 }
